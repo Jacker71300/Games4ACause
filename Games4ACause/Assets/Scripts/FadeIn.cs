df22fc505f6e30b8bf264c861e03644ff6869ecc
MGMolnar <55547115+MGMolnar@users.noreply.github.com>
2019-11-16 15:59:08 -0500

    Updated stuff
    Scenes change great now

--- Games4ACause/Assets/Scripts/FadeIn.cs
+++ Games4ACause/Assets/Scripts/FadeIn.cs
@@ -6,31 +6,40 @@ public class FadeIn : MonoBehaviour
 {
     private CanvasGroup canvasGroup;
     private float currentAlpha;
-    private bool fading;
+    public bool fading;
     public float fadeSpeed;
+    public float fadeStop;
+
+    public static FadeIn fadeInstance;
+
     // Start is called before the first frame update
     void Start()
     {
         canvasGroup = gameObject.GetComponent<CanvasGroup>();
         currentAlpha = 0;
         canvasGroup.alpha = 0;
-        fading = false;
+
+        if (fadeInstance == null)
+        {
+            fadeInstance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            fading = true;
-        }
         if(fading)
         {
             currentAlpha += Time.deltaTime * fadeSpeed;
-            if(currentAlpha > 0.5f)
+            if(currentAlpha > fadeStop)
             {
                 fading = false;
-                currentAlpha = 0.5f;
+                currentAlpha = fadeStop;
             }
         }
         canvasGroup.alpha = currentAlpha;
