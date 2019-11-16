4739a9f6fbc5b326592fa027605dbeb9a5cbf657
MGMolnar <55547115+MGMolnar@users.noreply.github.com>
2019-11-16 11:57:59 -0500

    Working on scene manager

--- Games4ACause/Assets/Scripts/SceneManager.cs
+++ Games4ACause/Assets/Scripts/SceneManager.cs
@@ -4,35 +4,44 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    public GameObject endLevelImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        endLevelImage = Instantiate(endLevelImage, new Vector3(0, 0, -9), Quaternion.identity);
+        endLevelImage.SetActive(false);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (endLevelImage.activeInHierarchy)
+        {
+            EndLevel();
+        }
     }
 
     public void EndLevel()
     {
+        endLevelImage.SetActive(true);
 
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) && endLevelImage.activeInHierarchy)
+        {
+            NextLevel();
+        }
     }
 
     public void NextLevel()
     {
-
     }
 
     public void RestartLevel()
     {
-
     }
 
     public void Menu()
     {
-
     }
 }
