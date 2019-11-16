a242e59bad180f68353c105c6ef6a0f147b6968b
MGMolnar <55547115+MGMolnar@users.noreply.github.com>
2019-11-15 23:39:07 -0500

    Color on ball Changes
    COLOR ON THE BALL CHANGES BASED ON WHAT ENUM IT IS.

--- Games4ACause/Assets/Scripts/ColorChanger.cs
+++ Games4ACause/Assets/Scripts/ColorChanger.cs
@@ -6,12 +6,35 @@ public class ColorChanger : MonoBehaviour
 {
     public CustomController.Mode mode;
 
+    private Material color;
+
     private Collider characterCollider;
+
+    public GameObject character;
+
+    private Material characterMaterial;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         characterCollider = GameObject.Find("Character").GetComponent<Collider>();
+        characterMaterial = character.GetComponent<Renderer>().material;
+
+        switch (mode)
+        {
+            case CustomController.Mode.Default:
+                color = Resources.Load("Materials/Black", typeof(Material)) as Material;
+                break;
+            case CustomController.Mode.Dense:
+                color = Resources.Load("Materials/Red", typeof(Material)) as Material;
+                break;
+            case CustomController.Mode.Jump:
+                color = Resources.Load("Materials/White", typeof(Material)) as Material;
+                break;
+        }
+
     }
 
     // Update is called once per frame
@@ -25,6 +48,8 @@ public class ColorChanger : MonoBehaviour
         if (characterCollider.bounds.Intersects(gameObject.GetComponent<Collider>().bounds))
         {
             CustomController.controllerInstance.mode = mode;
+            character.GetComponent<Renderer>().material = color;
+            
         }
 
     }
